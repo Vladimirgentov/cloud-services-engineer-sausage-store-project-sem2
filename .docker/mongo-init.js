7493def310058c89f3aea8e54424340ab6f89c1d
Vladimirgentov <v@gentov.ru>
2026-06-25 06:52:21 +0300

    Add docker compose for local sausage store

---
+++ .docker/mongo-init.js
@@ -0,0 +1,11 @@
+﻿db = db.getSiblingDB('sausage-store');
+db.createUser({
+  user: 'reports',
+  pwd: 'reportspassword',
+  roles: [
+    {
+      role: 'readWrite',
+      db: 'sausage-store'
+    }
+  ]
+});
